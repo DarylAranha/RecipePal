a65c7752648ad0840a93a09e2463cb7a34eab10b
darylaranha <daryl.aranha@gmail.com>
2023-03-30 14:56:24 -0400

    Added Navigational component

--- app/src/main/java/com/mdev/recipepal/MainActivity.kt
+++ app/src/main/java/com/mdev/recipepal/MainActivity.kt
@@ -2,57 +2,36 @@ package com.mdev.recipepal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.SearchView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 
 import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recipeAdapter: RecipeAdapter
-    private lateinit var recipeList: RecyclerView
-    private lateinit var tabLayout: TabLayout
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recipeList = findViewById(R.id.recipeList)
-        tabLayout = findViewById(R.id.tabLayout)
-        val searchView = findViewById<SearchView>(R.id.searchView)
-        recipeList.layoutManager = LinearLayoutManager(this)
-        recipeAdapter = RecipeAdapter(getRecipes())
-        recipeList.adapter = recipeAdapter
 
-//        tabLayout.addTab(tabLayout.newTab().setText("Home"))
-//        tabLayout.addTab(tabLayout.newTab().setText("Grocery List"))
-//        tabLayout.addTab(tabLayout.newTab().setText("User Info"))
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                recipeAdapter.filter.filter(newText)
-                return true
-            }
-        })
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigation.setupWithNavController(navController)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
 
-    private fun getRecipes(): List<Recipe> {
-        // Dummy data for the recipe list
-        val recipes = mutableListOf<Recipe>()
-        recipes.add(Recipe("Chicken Alfredo"))
-        recipes.add(Recipe("Beef Stir Fry"))
-        recipes.add(Recipe("Vegetable Curry"))
-        recipes.add(Recipe("Pesto Pasta"))
-        recipes.add(Recipe("Lentil Soup"))
-        recipes.add(Recipe("Salmon with Asparagus"))
-        return recipes
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
